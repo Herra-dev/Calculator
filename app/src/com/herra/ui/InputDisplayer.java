16526a328310005034860c32f41b21f84963462d
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-06 22:03:01 +0300

    creating the displayer: class inherits JLabel

---
+++ app/src/com/herra/ui/InputDisplayer.java
@@ -0,0 +1,39 @@
+package com.herra.ui;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.JLabel;
+
+import com.interfaces.sh.Observable;
+import com.interfaces.sh.Observer;
+
+public class InputDisplayer extends JLabel implements Observable{
+
+    protected List<Observer> _c_observer = new LinkedList<Observer>();
+
+    public InputDisplayer()
+    {
+        this.setHorizontalAlignment(RIGHT);
+    }
+
+//==========================================================================================
+
+    @Override public boolean _addObserver(Observer _observer) {
+        _c_observer.add(_observer);
+
+        return true;
+    }
+
+//==========================================================================================
+
+    @Override public boolean _updateObserver() {
+        for(Observer obs: _c_observer)
+            obs.update(obs);
+        return true;
+    }
+
+//==========================================================================================
+
+
+}
