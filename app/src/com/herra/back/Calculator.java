3f1212105c68ff02433dc7c285b67892308a7f3e
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-06 22:26:38 +0300

    implementation Calculator.java

--- app/src/com/herra/back/Calculator.java
+++ app/src/com/herra/back/Calculator.java
@@ -8,18 +8,23 @@ import com.interfaces.sh.Observer;
 
 public class Calculator implements Observer, Observable{
 
-    protected List<Observer> _cObservers = new LinkedList<Observer>(); 
+    protected List<Observer> _cObservers = new LinkedList<Observer>();
+    protected String _cNumbers = "";
 
     public boolean update(Object _obj) {
+        this._cNumbers = (String)_obj;
 
         return true;
     }
 
     public boolean _addObserver(Observer _observer) {
+        this._cObservers.add(_observer);
 
         return true;
     }
     public boolean _updateObserver() {
+        for(Observer obs: _cObservers)
+            obs.update(obs);
 
         return true;
     }
