11ee0cfcfd6949bb9a8a07812bbc434f48b699e8
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-09 19:30:57 +0300

    method to arrange user input

--- app/src/App.java
+++ app/src/App.java
@@ -3,7 +3,7 @@ import com.herra.back.Calculator;
 public class App {
     public static void main(String[] args) {
         @SuppressWarnings("unused")
-        Calculator calc = new Calculator("++++----array ary+---9");
+        Calculator calc = new Calculator("++++----array ary---9");
 
         // String str = new String("array ary");
         // System.out.println(str.replaceAll("[ar|ra]", "+"));
