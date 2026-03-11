217c59b5835c5ed920ec0d051f0c4ef27de7cd55
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-11 18:06:10 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -1,8 +1,10 @@
-import com.herra.back.Calculator;
+// import com.herra.back.Calculator;
 
 public class App {
     public static void main(String[] args) {
-        @SuppressWarnings("unused")
-        Calculator calc = new Calculator("-(9*+99.5(+-+-8))");
+        // @SuppressWarnings("unused")
+        // Calculator calc = new Calculator("-(9*+99.5(+-+-8))");
+
+        System.out.println((char)97);
     }
 }
