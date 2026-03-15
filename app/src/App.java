2b9fa2e96421448476b8146c6dc05ba7a4545ea7
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-15 20:23:55 +0300

    doc

--- app/src/App.java
+++ app/src/App.java
@@ -2,7 +2,8 @@ import com.herra.back.Calculator;
 
 public class App {
     public static void main(String[] args) {
-        Calculator calc = new Calculator("-9*+99.51+-+-8)");
+        Calculator calc = new Calculator("-9*+99.51+-+-8");
         calc._calcul(calc._separeInput());
+        System.out.println(calc._get_cOutput());
     }
 }
