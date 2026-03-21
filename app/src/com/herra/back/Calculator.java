03b7d991c31d7529b75497cbda3c8da40475c2b0
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-21 21:13:59 +0300

    wip

---
+++ app/src/com/herra/back/Calculator.java
@@ -0,0 +1,19 @@
+package com.herra.back;
+
+import com.interfaces.sh.Observable;
+import com.interfaces.sh.Observer;
+
+public class Calculator implements Observer, Observable {
+    @Override public boolean update(Object _obj) {
+        return true;
+    }
+
+    @Override public boolean _addObserver(Observer _observer) {
+        return true;
+    }
+    
+    @Override public boolean _updateObserver() {
+        return true;
+    }
+    
+}
