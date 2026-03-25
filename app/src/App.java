15157acfa01903b527c31a0a307da1ab924d11b8
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-25 15:12:13 +0300

    method to test if user input contains an invalid operation like division by zero, this method will raise an exception in case of error

--- app/src/App.java
+++ app/src/App.java
@@ -1,31 +1,13 @@
-// import java.util.regex.Matcher;
-// import java.util.regex.Pattern;
-
 import com.herra.back.DecimalCalculator;
 import com.herra.exception._SyntaxErrorException;
 
 public class App {
     public static void main(String[] args) {
-        DecimalCalculator dc = new DecimalCalculator("16/000000001+1/000");
+        DecimalCalculator dc = new DecimalCalculator("16/000000001+1/000010000");
         try {
             dc.testParenthesis();
         } catch (_SyntaxErrorException e) {
             e.printStackTrace();
         }   
-        
-        // Pattern p = Pattern.compile("aaaaab");
-        // Matcher m = p.matcher("aaaaab");
-        // boolean b = m.matches();
-
-        // if(b) System.out.println("true");
-        // else System.out.println("false");
-
-        // char character = '5';
-
-        // for(char i = '1'; i < '9'; i++) {
-        //     if(character == i) 
-        //         break;
-        //     System.out.println("i = " + (char)i + ", character = " + character);
-        // }
     }
 }
