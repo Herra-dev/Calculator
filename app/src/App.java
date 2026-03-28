05e3ce6efa6210045fe62068c2bb8bec7bb3702c
Heriniaina <irdnarheriniaina@gmail.com>
2026-03-28 21:26:13 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -4,7 +4,7 @@ import com.herra.back.DecimalCalculator;
 public class App {
     public static void main(String[] args) {
         @SuppressWarnings("unused")
-        DecimalCalculator dc = new DecimalCalculator("10/-5");
+        DecimalCalculator dc = new DecimalCalculator("-10/-5.2+0.08");
 
     }
 }
