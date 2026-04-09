7531650cce5d4c754e56f34dd0f3e3f84221530e
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-09 22:15:51 +0300

    wip

--- app/src/com/herra/ui/Calculator.java
+++ app/src/com/herra/ui/Calculator.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,7 +18,7 @@ public class Calculator extends JFrame implements KeyListener {
     protected LinkedList<String> key_list_operator = this.setKeyOperator();
     protected JPanel DisplayerPanel = new JPanel(new GridLayout(2, 1)); // two lines and one column
     protected JPanel NumberPanel = new JPanel(new GridLayout(4, 3)); // four lines and three columns
-    protected JPanel OperatorPanel = new JPanel(new GridLayout(5, 1));
+    protected JPanel OperatorPanel = new JPanel(new GridLayout(key_list_operator.size(), 1));
 
 //==================================================================================
 
@@ -25,14 +26,15 @@ public class Calculator extends JFrame implements KeyListener {
         this.setLocationRelativeTo(null);
         this.setTitle("Calculator");
         this.setSize(500, 500);
+        this.setMinimumSize(new Dimension(200, 200));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new GridLayout());
+        this.setLayout(new BorderLayout());
 
         this.addToPanel(NumberPanel, key_list_number);
         this.addToPanel(OperatorPanel, key_list_operator);
 
-        this.getContentPane().add(NumberPanel, BorderLayout.NORTH);
-        this.getContentPane().add(OperatorPanel, BorderLayout.WEST);
+        this.getContentPane().add(NumberPanel, BorderLayout.WEST);
+        this.getContentPane().add(OperatorPanel, BorderLayout.EAST);
 
         this.addKeyListener(this);
               
@@ -67,7 +69,6 @@ public class Calculator extends JFrame implements KeyListener {
         key.add("3");
         key.add("0");
         key.add(".");
-        key.add("%");
 
         return key;
     }
