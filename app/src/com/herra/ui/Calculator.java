f9a8e09c6ea46e87ec5b6871301ee983e25bd7d6
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-10 14:52:32 +0300

    wip

--- app/src/com/herra/ui/Calculator.java
+++ app/src/com/herra/ui/Calculator.java
@@ -3,10 +3,11 @@ package com.herra.ui;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,43 +17,51 @@ import java.util.LinkedList;
 
 public class Calculator extends JFrame implements KeyListener {
     protected LinkedList<String> key_list_number = this.setKeyNumber();
-    protected LinkedList<String> key_list_operator = this.setKeyOperator();
     protected JPanel DisplayerPanel = new JPanel(new GridLayout(2, 1)); // two lines and one column
-    protected JPanel NumberPanel = new JPanel(new GridLayout(key_list_number.size()/3, 3)); // four lines and three columns
-    protected JPanel OperatorPanel = new JPanel(new GridLayout(key_list_operator.size(), 1));
+    protected JPanel NumberPanel = new JPanel(new GridLayout(4, 4)); // four lines and three columns
 
 //==================================================================================
 
     public Calculator() {
+        this.setCalculatorProperty(); // set properties for the Calculator Window
+        
+
+        this.addButtonToPanel(NumberPanel, key_list_number);
+        this.getContentPane().add(NumberPanel, BorderLayout.CENTER);
+
+        this.addKeyListener(this);
+              
+        
+    }
+
+//==================================================================================
+
+    public void setCalculatorProperty() {
         this.setLocationRelativeTo(null);
         this.setTitle("Calculator");
         this.setSize(500, 500);
         this.setMinimumSize(new Dimension(200, 200));
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-
-        this.addToPanel(NumberPanel, key_list_number);
-        this.addToPanel(OperatorPanel, key_list_operator);
-
-        this.getContentPane().add(NumberPanel, BorderLayout.WEST);
-        this.getContentPane().add(OperatorPanel, BorderLayout.EAST);
-
-        JTextField field = new JTextField("text here");
-        this.getContentPane().add(field, BorderLayout.CENTER);
-
-        this.addKeyListener(this);
-              
         this.setFocusable(true);
         this.requestFocusInWindow();
         this.setVisible(true);
     }
 
-//==================================================================================
-
-    public void addToPanel(JPanel panel, LinkedList<String> to_add) {
+    public void addButtonToPanel(JPanel panel, LinkedList<String> to_add) {
         for(String str: to_add) {
             JButton bt = new JButton(str);
             bt.setFocusable(false);
+            bt.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    System.out.println("Button : " + bt.getText() + " clicked");
+                }
+            });
+            if(str.matches("[0-9]") || str.equals("."))
+                bt.setBackground(Color.CYAN);
+            
+            bt.setFont(new Font("Arial", 1, 50));
+
             panel.add(bt);
         }
     }
@@ -60,35 +69,12 @@ public class Calculator extends JFrame implements KeyListener {
 //==================================================================================
 
     public LinkedList<String> setKeyNumber() {
-        String key_number = "7894561230.%";
+        String key_number = "789+456-123*0.%/";
         LinkedList<String> key = new LinkedList<String>();
 
         for(int i = 0; i < key_number.length(); i++) key.add(key_number.charAt(i)+"");
 
         return key;
-    }
-
-//==================================================================================
-
-    public LinkedList<String> setKeyOperator() {
-        String key_operator = "+-*/()";
-        LinkedList<String> key = new LinkedList<String>();
-
-        for(int i = 0; i < key_operator.length(); i++) key.add(key_operator.charAt(i)+"");
-
-        return key;
-    }
-
-//==================================================================================
-
-    public LinkedList<String> getKeyNumber() {
-        return this.key_list_number;
-    }
-
-//==================================================================================
-
-    public LinkedList<String> getKeyOperator() {
-        return this.key_list_operator;
     }
 
 //==================================================================================
@@ -106,17 +92,5 @@ public class Calculator extends JFrame implements KeyListener {
     @Override public void keyReleased(KeyEvent event) { }
 
 //==================================================================================
-
-    public JButton addActionListener(JButton button) {
-        button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                System.out.println("you pressed the button : " + button.getText() + ", via addActionListener button");
-            }
-        });
-
-        return button;
-    }
-
-//==================================================================================    
 
 }
