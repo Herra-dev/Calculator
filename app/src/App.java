95fef462649e1adfc7bd22505cb9119f258dfee1
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-14 06:02:10 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         // Calculator calc = new Calculator();
         // calc.setVisible(true);
-        DecimalCalculator calc = new DecimalCalculator("(9)2");
+        DecimalCalculator calc = new DecimalCalculator("(9).0");
         System.out.println(calc.calcul());
     }
 }
