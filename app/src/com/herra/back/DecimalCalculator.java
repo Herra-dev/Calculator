a31e58c11596c6e091b70e7508671d167c55b46b
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-15 06:02:53 +0300

    wip

--- app/src/com/herra/back/DecimalCalculator.java
+++ app/src/com/herra/back/DecimalCalculator.java
@@ -4,7 +4,7 @@ public class DecimalCalculator extends AbstractDecimalCalculator{
 /**
  * <h3>Construct new DecimalCalculator</h3>
  * 
- * @param user_input
+ * @param user_input {@code String}
  */
     public DecimalCalculator(String user_input) {
         this._input = user_input;
