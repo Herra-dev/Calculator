62da8a76def00f27ed729de2bd91f867a3b82ccc
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-15 05:57:35 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -1,13 +1,13 @@
-import com.herra.ui.Calculator;
+// import com.herra.ui.Calculator;
 
-// import com.herra.back.DecimalCalculator;
+import com.herra.back.DecimalCalculator;
 
 public class App {
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        calc.setVisible(true);
-        // DecimalCalculator calc = new DecimalCalculator("2+*6");
-        // calc.calcul();
-        // System.out.println("output: " + calc.getOutput());
+        // Calculator calc = new Calculator();
+        // calc.setVisible(true);
+        DecimalCalculator calc = new DecimalCalculator("2+*6");
+        calc.calcul();
+        System.out.println("output: " + calc.getOutput());
     }
 }
