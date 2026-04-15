958e4363dd8cb4accaef99c9dd59ff88c7665b83
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-15 06:45:24 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -1,14 +1,14 @@
-// import com.herra.ui.Calculator;
+import com.herra.ui.Calculator;
 
-import com.herra.back.DecimalCalculator;
+// import com.herra.back.DecimalCalculator;
 
 public class App {
     public static void main(String[] args) {
-        // Calculator calc = new Calculator();
-        // calc.setVisible(true);
-        DecimalCalculator calc = new DecimalCalculator("9/9");
-        calc.calcul();
-        System.out.println("can proceed : " + calc.getAuthorization());
-        System.out.println("output: " + calc.getOutput());
+        Calculator calc = new Calculator();
+        calc.setVisible(true);
+        // DecimalCalculator calc = new DecimalCalculator("9---9");
+        // calc.calcul();
+        // System.out.println("can proceed : " + calc.getAuthorization());
+        // System.out.println("output: " + calc.getOutput());
     }
 }
