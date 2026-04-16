2aa791e19d0bbcc04d24c3482c7f1660992c62ac
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-16 04:55:04 +0300

    wip

--- app/src/App.java
+++ app/src/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         // Calculator calc = new Calculator();
         // calc.setVisible(true);
-        DecimalCalculator calc = new DecimalCalculator("-3/3");
+        DecimalCalculator calc = new DecimalCalculator("3+3/3");
         calc.calcul();
         System.out.println("can proceed : " + calc.getAuthorization());
         System.out.println("output: " + calc.getOutput());
