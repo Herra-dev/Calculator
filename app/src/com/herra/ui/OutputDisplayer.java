d6c0a94b72291953b9409b5e853483b0acce9c93
Heriniaina <irdnarheriniaina@gmail.com>
2026-04-16 06:33:02 +0300

    wip

--- app/src/com/herra/ui/OutputDisplayer.java
+++ app/src/com/herra/ui/OutputDisplayer.java
@@ -1,10 +1,14 @@
 package com.herra.ui;
 
+import java.awt.Color;
+
 import javax.swing.JLabel;
 
 import com.interfaces.sh.Observer;
 
 public class OutputDisplayer extends JLabel implements Observer{
+
+    protected String _errString = "SYNTAX ERROR";
 
     public OutputDisplayer() {
         this.setHorizontalAlignment(RIGHT);
@@ -14,6 +18,9 @@ public class OutputDisplayer extends JLabel implements Observer{
 //==========================================================================================
 
     @Override public boolean update(Object _obj) {
+        if(_obj.equals(_errString)) this.setForeground(Color.RED);
+        else                        this.setForeground(Color.BLACK);
+        
         this.setText((String)_obj);
 
         return true;
